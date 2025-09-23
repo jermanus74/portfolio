d18e26d75bf10b6ed96cc3f908c627300b104a54
Jermanus peter <133773488+jermanus74@users.noreply.github.com>
2025-09-23 18:48:22 +0300

    Update page.tsx

--- src/pages/404/page.tsx
+++ src/pages/404/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { Navigation } from "@/components/layout/navbar";
+// import { Navigation } from "@/components/layout/navbar";
 import { motion } from "framer-motion";
 import Link from "next/link";
 
 export default function NotFoundPage() {
   return (
     <>
-      <Navigation />
+      // <Navigation />
       <main
         className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4 relative overflow-hidden"
         style={{
