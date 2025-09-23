8b2166024808b91ae68b4e4dc8ce91baf9eb194d
Jermanus peter <133773488+jermanus74@users.noreply.github.com>
2025-09-23 20:52:55 +0300

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next"
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl ="https://jermanus-portfolio.vercel.app/"
+  const baseUrl ="https://portfolio.uzika.me/"
 
   return [
     {
