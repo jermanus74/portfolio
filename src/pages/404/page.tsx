ed851b8e816d32e25850ee8834e9f617c7aa1432
Jermanus peter <133773488+jermanus74@users.noreply.github.com>
2025-09-23 18:52:21 +0300

    Update page.tsx

--- src/pages/404/page.tsx
+++ src/pages/404/page.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 export default function NotFoundPage() {
   return (
     <>
-      // <Navigation />
+      {/* <Navigation /> */}
       <main
         className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4 relative overflow-hidden"
         style={{
