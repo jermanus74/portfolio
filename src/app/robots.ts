6337d1c3d9925624e681987a69afcbcee15478a8
Jermanus peter <133773488+jermanus74@users.noreply.github.com>
2025-09-23 19:26:20 +0300

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://jermanus.dev";
+  const baseUrl = "https://jermanus-portfolio.vercel.app/";
 
   return {
     rules: {
