10a24f9eaa5234ba15a1c426987f49d052315d66
Jermanus peter <133773488+jermanus74@users.noreply.github.com>
2025-09-23 21:35:00 +0300

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next"
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl ="https://portfolio.uzika.me/"
+  const baseUrl ="https://portfolio.uzika.me"
 
   return [
     {
